2cb406e8667e4a271bb17b007d99cd6b7619e8b8
hoon <1bun0403@gmail.com>
2021-06-13 14:03:06 +0900

    Hello World 추가

---
+++ src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.example.restfulwebservice.helloworld;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloWorldController {
+    // @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+}
